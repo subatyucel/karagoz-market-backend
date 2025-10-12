0c37d1fe18ba2e4919fe20ba235d9902f62f3ba8
subat <subat.yucel1@gmail.com>
2025-10-12 23:53:30 +0300

    notFoundHandler created

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { getEnv } from 'utils/getEnv';
 import { errorHandler } from 'middlewares/errorHandler';
+import { notFoundHandler } from 'middlewares/notFoundHandler';
 
 const PORT = +getEnv('PORT', '3000');
 const app = express();
@@ -10,6 +11,7 @@ export const setupServer = () => {
   app.use(cors());
   app.use(express.json());
 
+  app.use('*', notFoundHandler);
   app.use(errorHandler);
   app.listen(PORT, () => console.log(`App is running on port: ${PORT}`));
 };
