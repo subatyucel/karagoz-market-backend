43f21b66b021c04576b55117074e39621ee00e24
subat <subat.yucel1@gmail.com>
2025-10-13 16:21:35 +0300

    add product validation implemented

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,9 +1,15 @@
 import { addProductController } from 'controllers/product';
 import { Router } from 'express';
+import { bodyValidator } from 'middlewares/bodyValidator';
 import { asyncWrapper } from 'utils/asyncWrapper';
+import { addProductSchema } from 'validation/product';
 
 const router = Router();
 
-router.post('/add', asyncWrapper(addProductController));
+router.post(
+  '/add',
+  bodyValidator(addProductSchema),
+  asyncWrapper(addProductController),
+);
 
 export default router;
---
+++ src/validation/product.ts
@@ -0,0 +1,26 @@
+import Joi from 'joi';
+
+export const addProductSchema = Joi.object({
+  barcode: Joi.string()
+    .trim()
+    .pattern(/^(?:\d{8}|\d{12}|\d{13})$/)
+    .required()
+    .messages({ '*': 'Ürünün barkod bilgisi olması gerekir!' }),
+  brand: Joi.string().min(3).max(50).required().messages({
+    '*': 'Ürün marka bilgisi olması gerekir!',
+  }),
+  name: Joi.string()
+    .min(2)
+    .max(50)
+    .required()
+    .messages({ '*': 'Ürün isim bilgisi olması gerekir!' }),
+  price: Joi.number().positive().precision(2).required().messages({
+    '*': 'Ürün fiyatı pozitif ve en fazla 2 ondalıklı bir sayı olması gerekir!',
+  }),
+  photo: Joi.string()
+    .trim()
+    .optional()
+    .messages({ '*': 'Fotoğraf geçerli bir URL olmalıdır!' }),
+})
+  .required()
+  .messages({ '*': 'Ürün bilgileri paylaşılmalıdır!' });
