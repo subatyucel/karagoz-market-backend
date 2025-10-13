3987b065b5ea828ab2447f4b8c2a108461d44225
subat <subat.yucel1@gmail.com>
2025-10-13 22:17:08 +0300

    getProducts functionality added

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,6 +1,11 @@
 import type { IProduct } from 'db/models/product';
 import type { Request, Response } from 'express';
-import { addProduct, deleteProduct, updateProduct } from 'services/product';
+import {
+  addProduct,
+  deleteProduct,
+  getProducts,
+  updateProduct,
+} from 'services/product';
 import { createSuccessResponse } from 'utils/createResponse';
 
 export const addProductController = async (
@@ -28,4 +33,25 @@ export const updateProductController = async (req: Request, res: Response) => {
   res
     .status(200)
     .json(createSuccessResponse('Ürün başarıyla güncellendi!', updatedProduct));
+};
+
+export const getProductsController = async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.page as string) || 10;
+  const filters = {
+    category: (req.query.category as string) || undefined,
+    search: (req.query.search as string) || undefined,
+    minPrice: req.query.minPrice
+      ? parseFloat(req.query.minPrice as string)
+      : undefined,
+    maxPrice: req.query.maxPrice
+      ? parseFloat(req.query.maxPrice as string)
+      : undefined,
+  };
+
+  const products = await getProducts(page, limit, filters);
+
+  res
+    .status(200)
+    .json(createSuccessResponse('Ürünler başarıyla getirildi', products));
 };
--- src/services/product.ts
+++ src/services/product.ts
@@ -5,6 +5,26 @@ import {
 } from 'db/models/product';
 import createHttpError from 'http-errors';
 
+interface ProductFilters {
+  category?: string;
+  search?: string;
+  minPrice?: number;
+  maxPrice?: number;
+  quantity?: number;
+}
+
+interface PaginationResult {
+  data: IProductDocument[];
+  pagination: {
+    currentPage: number;
+    totalPages: number;
+    totalItems: number;
+    itemsPerPage: number;
+    hasNextPage: boolean;
+    hasPrevPage: boolean;
+  };
+}
+
 export const addProduct = async (
   payload: IProduct,
 ): Promise<IProductDocument> => {
@@ -38,4 +58,47 @@ export const updateProduct = async (id: string, payload: Partial<IProduct>) => {
   if (!product) throw createHttpError(404, 'Bu id hiç bir ürüne ait değil!');
 
   return product;
+};
+
+export const getProducts = async (
+  page: number,
+  limit: number,
+  filters: ProductFilters = {},
+): Promise<PaginationResult> => {
+  const skip = (page - 1) * limit;
+  const query: any = {};
+
+  if (filters.category) query.category = filters.category;
+  if (filters.search)
+    query.$or = [
+      { name: { $regex: filters.search, $options: 'i' } },
+      { barcode: { $regex: filters.search, $options: 'i' } },
+    ];
+
+  if (filters.minPrice || filters.maxPrice) {
+    query.price = {};
+    if (filters.minPrice) query.price.$gte = filters.minPrice;
+    if (filters.maxPrice) query.price.$lte = filters.maxPrice;
+  }
+
+  const total = await Product.countDocuments(query);
+  const products = await Product.find(query).skip(skip).limit(limit);
+
+  if (!products) throw createHttpError(404, 'Hiç ürün bulunamadı!');
+
+  const totalPages = Math.ceil(total / limit);
+  const hasNextPage = page < totalPages;
+  const hasPrevPage = page > totalPages;
+
+  return {
+    data: products,
+    pagination: {
+      currentPage: page,
+      totalPages,
+      totalItems: total,
+      itemsPerPage: limit,
+      hasNextPage,
+      hasPrevPage,
+    },
+  };
 };
